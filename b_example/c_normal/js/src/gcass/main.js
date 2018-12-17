ed1390411520d92e5ed0b076e50add56562a6123
taehoon9410 <a92109410@gmail.com>
2018-12-17 17:57:15 +0900

    20181217

---
+++ b_example/c_normal/js/src/gcass/main.js
@@ -0,0 +1,15 @@
+// main.js\
+(function($){
+  console.log('!!!!!');
+  var htmlcommonAreaBox = './commonBox';
+  var homeBox=$('homeBox');
+  var headBox=$('headBox');
+  var footBox=$('footBox');
+  //load()메소드는 반드시 무조건 꼭 서버로 구동되야만 동작한다.
+  //외부에 존재하는 문서를 불러와서 담는 기능
+  //html 문서의 경우 기준 상대경로는 삽입하려고 하는 문서(html)을
+  //기준으로 한다.
+  homeBox.load( htmlcommonAreaBox + 'homeBox.html');
+  headBox.load( htmlcommonAreaBox + 'headBox.html');
+  footBox.load( htmlcommonAreaBox + 'footBox.html');
+})(jQuery);
